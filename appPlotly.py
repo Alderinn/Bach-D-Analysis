725cf1ab09e74c975f28d4211f0255032539fb4a
Alderinn <dmancini199@gmail.com>
2023-08-15 12:51:13 -0400

    made a better graph app

---
+++ appPlotly.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import plotly.express as px
+
+
+def createBubbles():
+    # Load data from CSV file
+    data = pd.read_csv("reults.csv")
+    # Create a bubble chart
+    fig = px.scatter(data, x="episode", y="season",
+                    size="count", color="count",
+                    hover_name="count", log_x=True, size_max=60,
+                    title="Bubble Chart of Episode Counts by Season")
+    fig.update_layout(
+        xaxis_title="Episode",
+        yaxis_title="Season",
+        legend_title="Season"
+    )
+    fig.show()
--- scanForString.py
+++ scanForString.py
@@ -1,9 +1,8 @@
-import mmap, os, csv
 # The goal of this is to scan a directory for all of the txt files one at a time for a string
 # then, save the findings to a csv to later be used on a graph
 #CSV: season,episode,count
+import mmap, os, csv
 count=0
-
 def searchData(directory='./data/Bachelorette'):
     phrase=input("Search term:") # Ex: 'for the right reason'
     for filename in os.scandir(directory):
@@ -13,40 +12,22 @@ def searchData(directory='./data/Bachelorette'):
         
         if filename.is_file():
             filename = directory +'/'+filename.name
-            process(filename, phrase, count)
-            
+            found_count = process(filename, phrase, count)
             # {episodeNum},{seasonNum},{Occurance}
             with open('reults.csv','a') as f:
                 write = csv.writer(f)
-                write.writerow({seasonNum: 'seasons', episodeNum: 'episode', count:'count'})
+                write.writerow({seasonNum: 'seasons', episodeNum: 'episode', found_count:'count'})
                 print(f"Row for S{seasonNum}:E{episodeNum} added!")
-
-
-
 def process(filename,searchTerm,count):
     # find {searchTerm} within {filename} and return {count}
         try:
             with open(filename, 'r') as file:
                 content = file.read()
                 found_count = content.count(searchTerm)
-                return found_count == count
+                print(f"Found: {found_count}")
+                return found_count
         except FileNotFoundError:
             print(f"File '{filename}' not found.")
             return False
-"""
-
-    with open(filename, 'rb', 0) as file:
-        #- create a memory map of the whole file
-        s = mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ)
-        print('Searching for: '+ searchTerm)
-        searchTermb = bytes(searchTerm, encoding='utf-8')
-
-        #TODO: Get count working
-        if s.find(searchTermb) != -1:
-            print(f'Found! in {filename}')
-            count= count+1
-    return count
-"""
-
 # - Start
 searchData()
